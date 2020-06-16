51da97b0fd3c13eed1952d8f37428c1b0136df8f
rasmusbergpalm <rasmusbergpalm@gmail.com>
2020-06-16 15:58:34 +0200

    nbody vsmc

--- lgss_example.py
+++ lgss_example.py
@@ -221,7 +221,7 @@ if __name__ == '__main__':
         progress.update()
         if iter % 10 == 0:
             model_params, prop_params = combined_params
-            elbo = objective(combined_params, iter)
+            elbo = -objective(combined_params, iter)
             lml = log_marginal_likelihood(model_params, T, y_true)
             train_writer.add_scalar("loss/elbo", elbo, global_step=iter)
             train_writer.add_scalar("loss/logpx", lml, global_step=iter)
